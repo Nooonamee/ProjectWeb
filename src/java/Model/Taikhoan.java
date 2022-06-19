0f6d362f3c9dcd98e2222e9a2951516f4a119fd0
nguy2311 <88625401+nguy2311@users.noreply.github.com>
2022-06-19 15:55:55 +0700

    Update Taikhoan.java

--- src/java/Model/Taikhoan.java
+++ src/java/Model/Taikhoan.java
@@ -6,63 +6,83 @@
 package Model;
 
 
-public class Taikhoan {
-    private String hoten,tendn,email,mk,nlmk;
+public class taiKhoan {
+    private String id,ten,user_name,password,soDienThoai,diaChi,loai;
 
-    public Taikhoan() {
+    public taiKhoan() {
+    }
+
+    public taiKhoan(String id, String ten, String user_name, String password, String soDienThoai, String diaChi, String loai) {
+        this.id = id;
+        this.ten = ten;
+        this.user_name = user_name;
+        this.password = password;
+        this.soDienThoai = soDienThoai;
+        this.diaChi = diaChi;
+        this.loai = loai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getLoai() {
+        return loai;
+    }
+
+    public void setLoai(String loai) {
+        this.loai = loai;
+    }
+
+    @Override
+    public String toString() {
+        return "taiKhoan{" + "id=" + id + ", ten=" + ten + ", user_name=" + user_name + ", password=" + password + ", soDienThoai=" + soDienThoai + ", diaChi=" + diaChi + ", loai=" + loai + '}';
     }
     
-    public Taikhoan(String hoten, String tendn, String email, String mk, String nlmk) {
-        this.hoten = hoten;
-        this.tendn = tendn;
-        this.email = email;
-        this.mk = mk;
-        this.nlmk = nlmk;
-    }
-
-    public Taikhoan(String tendn, String mk) {
-        this.tendn = tendn;
-        this.mk = mk;
-    }
-
-    public String getHoten() {
-        return hoten;
-    }
-
-    public void setHoten(String hoten) {
-        this.hoten = hoten;
-    }
-
-    public String getTendn() {
-        return tendn;
-    }
-
-    public void setTendn(String tendn) {
-        this.tendn = tendn;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMk() {
-        return mk;
-    }
-
-    public void setMk(String mk) {
-        this.mk = mk;
-    }
-
-    public String getNlmk() {
-        return nlmk;
-    }
-
-    public void setNlmk(String nlmk) {
-        this.nlmk = nlmk;
-    }
+    
     
 }
